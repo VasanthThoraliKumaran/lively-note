bd32d4b0744591b10aeb2e722e6c4066d2439500
VasanthThorazhiKumaran <vasanthtk737@gmail.com>
2022-10-19 18:48:16 +0530

    init-starter-setup

--- lively-note-server/src/main.ts
+++ lively-note-server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import morgan = require('morgan');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(morgan('combined', {immediate: true}));
+  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
